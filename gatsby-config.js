ba3504f690587776bb7514f4f6a8fc5ddc3bde5a
Sidney Elmer <7726838+spelmer@users.noreply.github.com>
2020-04-07 17:35:22 -0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: process.env.UA-163100169-1,
       },
     },
     {
